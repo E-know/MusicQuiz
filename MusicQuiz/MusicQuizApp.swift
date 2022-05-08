6f3066fc90d84153077841e3a8de5f41d3fa6845
Inho Choi <eknow25@gmail.com>
2022-05-08 15:06:44 +0900

    Initial Commit

---
+++ MusicQuiz/MusicQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicQuizApp.swift
+//  MusicQuiz
+//
+//  Created by Choi Inho on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct MusicQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
