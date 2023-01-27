08dbd116b33ebd6553c46fa863fd6f416d085636
Inho Choi <eknow25@kakao.com>
2023-01-27 21:36:05 +0900

    [시작] 프로젝트 재설정

---
+++ MusicQuiz/MusicQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicQuizApp.swift
+//  MusicQuiz
+//
+//  Created by Inho Choi on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct MusicQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
